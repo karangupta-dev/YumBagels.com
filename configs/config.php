699e9d090bee5a83099a1dad650be6a6644286cf
karangupta-dev <karangupta-dev@gmail.com>
2016-04-05 19:56:59 +0530

    Author: Karan Gupta Config files that contain database connectivity credentials.

---
+++ configs/config.php
@@ -0,0 +1,32 @@
+<?php
+//ob_start();
+session_start();
+
+//database credentials
+define('DBHOST','localhost');
+define('DBUSER','root');
+define('DBPASS','');
+define('DBNAME','cafe');
+
+//image directory
+$dir = "uploads/daily_images/";
+
+
+//define('DBHOST','localhost');
+//define('DBUSER','u865672332_admin');
+//define('DBPASS','yc6NQWycSD');
+//define('DBNAME','u865672332_db1');
+
+try {
+
+	//create PDO connection 
+	//$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+	$db = new PDO("mysql:host=".DBHOST.";port=3307;dbname=".DBNAME, DBUSER, DBPASS);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+	//show error
+    echo $e->getMessage();
+	exit;
+}
+?>
